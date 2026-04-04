dafaed7f9245d8810f1fe74bfe6696030a2734a5
jeffKim <kwangho5538@gmail.com>
2026-04-04 18:05:47 +0900

    style: GuestCounter CSS Modules 스타일 적용

--- src/components/GuestCounter.jsx
+++ src/components/GuestCounter.jsx
@@ -1,16 +1,24 @@
+import styles from "./GuestCounter.module.css";
+
 function GuestCounter({ label, description, count, onChange }) {
   return (
-    <div>
-      <div>
-        <span>{label}</span>
-        <span>{description}</span>
+    <div className={styles.container}>
+      <div className={styles.labels}>
+        <span className={styles.label}>{label}</span>
+        <span className={styles.description}>{description}</span>
       </div>
-      <div>
-        <button onClick={() => onChange(count - 1)} disabled={count <= 0}>
+      <div className={styles.controls}>
+        <button
+          className={styles.button}
+          onClick={() => onChange(count - 1)}
+          disabled={count <= 0}
+        >
           -
         </button>
-        <span>{count}</span>
-        <button onClick={() => onChange(count + 1)}>+</button>
+        <span className={styles.count}>{count}</span>
+        <button className={styles.button} onClick={() => onChange(count + 1)}>
+          +
+        </button>
       </div>
     </div>
   );
