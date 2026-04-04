ce9da447aaa52d72a0ea43d22c234f8a2f21cbb6
myori <alinwinskingsleigh@gmail.com>
2026-04-04 18:33:36 +0900

    chore: src 루트 파일 세팅

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+// App.tsx 임시로
+import GuestCounter from "./components/GuestCounter";
+
+export default function App() {
+  return (
+    <GuestCounter name="성인" description="13세 이상" />
+  );
+}
---
+++ src/components/GuestCounter.tsx
@@ -0,0 +1,24 @@
+
+import { useState } from "react";
+
+type GuestCounterProps = {
+    name: string;
+    description: string;
+};
+
+export default function GuestCounter({name, description}:GuestCounterProps) {
+    const [count, setCount] = useState(0);
+
+    return (
+        <div>
+            <span>{name}, {description}</span>
+            <div>
+                <button type = "button" disabled = {count === 0} onClick={() => setCount(count - 1)}>-</button>
+                {count}
+                <button type = "button" onClick={() => setCount(count + 1)}>+</button>
+            </div>
+
+        </div>
+    );
+}
+
