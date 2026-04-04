d32bf6a7836de9efa0cd94d0126d1f31b12cf434
고민재 <minjae_koh@snu.ac.kr>
2026-04-04 10:04:29 +0900

    프로젝트 초기 환경 세팅 및 리액트 패키지 설치

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div>
+      <h1>안녕 에어비앤비!</h1>
+      <p>백지에서 시작하는 리액트 성공!</p>
+    </div>
+  );
+}
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+
+// HTML에 있던 id="root"를 찾아서 그 안에 App 컴포넌트를 렌더링합니다.
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
