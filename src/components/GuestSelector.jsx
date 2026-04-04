c5ac093cfa6c4a9c35d4eb173350f8026ea1cdec
jeffKim <kwangho5538@gmail.com>
2026-04-04 18:06:02 +0900

    style: GuestSelector CSS Modules 스타일 적용

--- src/components/GuestSelector.jsx
+++ src/components/GuestSelector.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import GuestModal from "./GuestModal";
+import styles from "./GuestSelector.module.css";
 
 function GuestSelector() {
   const [isOpen, SetIsOpen] = useState(false);
 
   return (
-    <div>
+    <div className={styles.container}>
       <button onClick={() => SetIsOpen(!isOpen)}>Who</button>
       {isOpen && <GuestModal />}
     </div>
