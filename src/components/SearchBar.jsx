356ffd781ba046024ef3488b5c86de7f77388ac6
jeffKim <kwangho5538@gmail.com>
2026-04-04 17:41:50 +0900

    feat: SearchBar 컴포넌트 추가 및 App 조립

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,13 @@
+import GuestSelector from "./GuestSelector";
+
+function SearchBar() {
+  return (
+    <div>
+      <button>Where</button>
+      <button>When</button>
+      <GuestSelector />
+    </div>
+  );
+}
+
+export default SearchBar;
