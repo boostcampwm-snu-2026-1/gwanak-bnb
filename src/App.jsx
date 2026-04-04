1ee270602db688129b907403cc81e2538eda5e93
jj1kim <jeewonbob@gmail.com>
2026-04-05 03:06:51 +0900

    feat: App에서 숙소 카드 리스트 렌더링

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Header from './components/Header/Header'
 import SearchBar from './components/SearchBar/SearchBar'
+import AccommodationCard from './components/AccommodationCard/AccommodationCard'
+import accommodations from './data/accommodations'
 import './App.css'
 
 function App() {
@@ -8,6 +10,11 @@ function App() {
       <Header />
       <main className="main">
         <SearchBar />
+        <section className="accommodation-list">
+          {accommodations.map((item) => (
+            <AccommodationCard key={item.id} accommodation={item} />
+          ))}
+        </section>
       </main>
     </div>
   )
