805fdf5ba42912f1ea6ce41811be11311053ab82
Jaewoong Choi <jwchoi.tech@gmail.com>
2026-04-05 19:02:27 +0900

    chore: modify description

--- src/constants/guest.ts
+++ src/constants/guest.ts
@@ -2,5 +2,9 @@ export const GUEST_TYPES = [
   { category: "adult", title: "성인", description: "13세 이상" },
   { category: "child", title: "어린이", description: "2~12세" },
   { category: "infant", title: "유아", description: "2세 미만" },
-  { category: "pet", title: "반려동물", description: "보조동물 동반" },
+  {
+    category: "pet",
+    title: "반려동물",
+    description: "보조동물을 동반하시나요?",
+  },
 ] as const;
