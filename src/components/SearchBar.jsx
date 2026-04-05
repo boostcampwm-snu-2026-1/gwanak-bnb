f9fadeb13609c653717a7e5d20d20997397184c3
정현우 <exile1111@snu.ac.kr>
2026-04-05 22:56:29 +0900

    component 분리
    Header, SearchBar, TravelerPopup으로 component를 분리하고, TravelerPopup을 수정하기 쉽도록 CounterRow를 작성

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import TravelerPopup from './TravelerPopup' // 여행자 팝업 컴포넌트
+
+function SearchBar() {
+  const [isPopupOpen, setIsPopupOpen] = useState(false)
+
+  const togglePopup = () => {
+    setIsPopupOpen(!isPopupOpen)
+  }
+
+  return (
+    <div className="search-bar">
+      <div className="spot">
+        <h2>여행지</h2>
+        <span className="placeholder">여행지 검색</span>
+      </div>
+      
+      <div className="divider"></div>
+
+      <div className="date">
+        <h2>날짜</h2>
+        <span className="placeholder">날짜 추가</span>
+      </div>
+
+      <div className="divider"></div>
+
+      <div className="traveler" onClick={togglePopup}>
+        <div className="traveler-text">
+          <h2>여행자</h2>
+          <span className="placeholder">게스트 추가</span>
+        </div>
+        
+        {isPopupOpen && <TravelerPopup />}
+      </div>
+
+        <button className="search-btn">🔍</button>
+    </div>
+
+  )
+}
+
+export default SearchBar;
