69a6cd00ea2f542db34cdf0d1db9f3db124d5341
a01034972144-hash <a01034972144@gmail.com>
2026-04-12 04:32:49 +0900

    검색창에서 날짜가 없어졌길래 날짜 추가(기능 구현X)

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -5,6 +5,9 @@ function Searchbar() {
   return (
     <div className="relative flex items-center border rounded-full shadow-md px-4 py-2 gap-4">
       <SearchLocation />
+      <button className="text-sm font-semibold px-4 border-r">
+        날짜
+      </button>
       <SearchGuest />
       <button className="bg-rose-500 text-white rounded-full p-2">🔍</button>
     </div>
