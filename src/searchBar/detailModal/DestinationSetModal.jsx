f5a04f948573f4cf353372f0b7e22b80f23af861
김하람 <gim222932@gmail.com>
2026-04-12 23:58:16 +0900

    chore: UI 개선

--- src/searchBar/detailModal/DestinationSetModal.jsx
+++ src/searchBar/detailModal/DestinationSetModal.jsx
@@ -1,11 +1,13 @@
+import styles from "../../css/SetModal.module.css";
+
 function DestinationSetModal({ queryDestination, setDestination }) {
   return (
-    <div>
+    <div className={styles.container}>
       {queryDestination.map((item) => (
-        <button key={item.name} onClick={() => setDestination(item.name)}>
-          <div>{item.name}</div>
-          <div>{item.country}</div>
-          <div>{item.description}</div>
+        <button key={item.name} onClick={() => setDestination(item.name)} className={styles.unit}>
+          <div className={styles.title}>{item.name}</div>
+          <div className={styles.desc}>{item.country}</div>
+          <div className={styles.desc}>{item.description}</div>
         </button>
       ))}
     </div>
