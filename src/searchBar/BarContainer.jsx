b80fc5d11176e60ff84e217a3e62ac12dac2c8c2
김하람 <gim222932@gmail.com>
2026-04-12 21:29:03 +0900

    feat: 게스트 선택에 따른 텍스트 변화 및 반려동물, 유아 추가 시 성인 인원 자동 설정 기능 구현 어려운 점: 어디에서 어떤 props를 넘겨야 하는지 설계하는 데에 시간이 걸렸다. 또 어떤 hook을 사용하는 게 맞는지 고민이 되었다.

--- src/searchBar/BarContainer.jsx
+++ src/searchBar/BarContainer.jsx
@@ -2,19 +2,20 @@ import { useState } from "react"
 import Destination from "./Destination"
 import Date from "./Date"
 import Traveler from "./Traveler"
-import TravelerSetModal from "./TravelerSetModal"
+import TravelerSetModal from "./detailModal/TravelerSetModal"
 import styles from "../css/BarContainer.module.css"
 
 function BarContainer() {
   const [isTravelModalOpen, setIsTravelModalOpen] = useState(false);
+  const [travelerCount, setTravelerCount] = useState({ adults: 0, children: 0, infants: 0, pets: 0 });
   return ( 
     <div>
       <div className={styles.container}>
           <Destination />
           <Date />
-          <Traveler setIsOpen={setIsTravelModalOpen}/>
+          <Traveler travelerCount={travelerCount} setIsOpen={setIsTravelModalOpen}/>
       </div>
-      {isTravelModalOpen && <TravelerSetModal />}
+      {isTravelModalOpen && <TravelerSetModal travelerCount={travelerCount} setTravelerCount={setTravelerCount} />}
     </div>
   )
 }
