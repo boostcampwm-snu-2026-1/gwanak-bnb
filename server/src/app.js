ccc809ad49d8eb0435341ff1ef8042ca14ed4156
Jaewan Park <pona4321@snu.ac.kr>
2026-04-19 02:05:00 +0900

    refactor: split project into client and server

---
+++ server/src/app.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+const clientUrl = process.env.CLIENT_URL || 'http://localhost:5173'
+
+app.use(
+  cors({
+    origin: clientUrl,
+    credentials: true,
+  }),
+)
+app.use(express.json())
+
+export default app
