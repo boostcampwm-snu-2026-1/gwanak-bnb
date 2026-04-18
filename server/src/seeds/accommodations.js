75f3cd1d8236f5fec827dae4857d6ff647f6c3e1
Jaewan Park <pona4321@snu.ac.kr>
2026-04-19 03:33:41 +0900

    feat: add accommodation seed data and seed script

---
+++ server/src/seeds/accommodations.js
@@ -0,0 +1,106 @@
+export const ACCOMMODATION_SEEDS = [
+  {
+    title: '광안대교 뷰 오션 스테이',
+    address: {
+      country: '대한민국',
+      city: '부산',
+      detail: '수영구 민락동',
+    },
+    pricePerNight: 149000,
+    rating: 4.92,
+    reviewCount: 128,
+    imageUrls: ['https://picsum.photos/id/1043/1200/800'],
+    guestCapacity: {
+      primary: { min: 1, max: 4 },
+      infant: { min: 0, max: 1 },
+      pet: { min: 0, max: 1 },
+    },
+  },
+  {
+    title: '해운대 센트럴 레지던스',
+    address: {
+      country: '대한민국',
+      city: '부산',
+      detail: '해운대구 우동',
+    },
+    pricePerNight: 172000,
+    rating: 4.88,
+    reviewCount: 94,
+    imageUrls: ['https://picsum.photos/id/1048/1200/800'],
+    guestCapacity: {
+      primary: { min: 1, max: 6 },
+      infant: { min: 0, max: 2 },
+      pet: { min: 0, max: 0 },
+    },
+  },
+  {
+    title: '제주 돌담 감성 하우스',
+    address: {
+      country: '대한민국',
+      city: '제주',
+      detail: '제주시 애월읍',
+    },
+    pricePerNight: 198000,
+    rating: 4.95,
+    reviewCount: 203,
+    imageUrls: ['https://picsum.photos/id/1050/1200/800'],
+    guestCapacity: {
+      primary: { min: 2, max: 5 },
+      infant: { min: 0, max: 2 },
+      pet: { min: 0, max: 1 },
+    },
+  },
+  {
+    title: '북촌 한옥 스테이',
+    address: {
+      country: '대한민국',
+      city: '서울',
+      detail: '종로구 가회동',
+    },
+    pricePerNight: 239000,
+    rating: 4.97,
+    reviewCount: 76,
+    imageUrls: ['https://picsum.photos/id/1068/1200/800'],
+    guestCapacity: {
+      primary: { min: 1, max: 4 },
+      infant: { min: 0, max: 1 },
+      pet: { min: 0, max: 0 },
+    },
+  },
+  {
+    title: '도쿄 시부야 컴팩트 아파트',
+    address: {
+      country: '일본',
+      city: '도쿄',
+      detail: '시부야구 진구마에',
+    },
+    pricePerNight: 184000,
+    rating: 4.84,
+    reviewCount: 142,
+    imageUrls: ['https://picsum.photos/id/1031/1200/800'],
+    guestCapacity: {
+      primary: { min: 1, max: 3 },
+      infant: { min: 0, max: 1 },
+      pet: { min: 0, max: 0 },
+    },
+  },
+  {
+    title: '오사카 난바 패밀리 룸',
+    address: {
+      country: '일본',
+      city: '오사카',
+      detail: '주오구 난바센니치마에',
+    },
+    pricePerNight: 161000,
+    rating: 4.79,
+    reviewCount: 111,
+    imageUrls: ['https://picsum.photos/id/1018/1200/800'],
+    guestCapacity: {
+      primary: { min: 2, max: 6 },
+      infant: { min: 0, max: 2 },
+      pet: { min: 0, max: 0 },
+    },
+  },
+]
+
+export default ACCOMMODATION_SEEDS
