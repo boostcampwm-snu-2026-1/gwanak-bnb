e35cdf88c58fe765d427e57bc4f8204905c4283e
김하람 <gim222932@gmail.com>
2026-04-19 20:08:11 +0900

    refactor: Refactor accommodation data schema 어려운 점: 어느 범위까지 필드를 설정할지 고민이 되었다. 특히 주소 필드를 설계할 때 한국의 주고 체계를 따로 설정할지, 아니면 나라에 구별없이 한 번에 통일할 지 고민이 되었다.

--- server/models/Destination.js
+++ server/models/Destination.js
@@ -3,10 +3,32 @@ import mongoose from "mongoose";
 const destinationSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, trim: true },
-    country: { type: String, required: true, trim: true },
+    address: {
+      country: { type: String, required: true, trim: true },
+      recipientName: { type: String },
+      phone: { type: String, trim: true },
+
+      fullAddress: { type: String, required: true },
+      detailAddress: { type: String },
+      city: {type: String},
+      stateProvince: {type: String},
+      postalCode: {type: String},
+      latitude: { type: Number },
+      longitude: { type: Number }      
+    },
     description: { type: String, default: "" },
     tags: { type: [String], default: [] },
-    popularity: { type: Number, default: 0 }
+    popularity: { type: Number, default: 0 },
+    capacity: { type: Number, default: 1 },
+    price: {
+      min: { type: Number, default: 0 },
+      max: { type: Number, default: 0 }
+    },
+    rating: {
+      average: { type: Number, default: 0 },
+      count: { type: Number, default: 0 }
+    },
+    images: { type: [String], default: ["/image/defaultimg.png"] }
   },
   { timestamps: true }
 );
